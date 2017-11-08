e939a167786de71c446d3ab65d628ee0421a88e1
kmesfun <kmess024@yahoo.com>
2017-11-07 16:51:16 -0800

    remove api

--- index.js
+++ index.js
@@ -20,40 +20,40 @@ app.get('/', function(req, res) {
 
 //yahoo finance
 
-var yahooFinance = require('yahoo-finance');
+// var yahooFinance = require('yahoo-finance');
 
 
-var SYMBOLS = [
-  'NASDAQ:AAPL',
-  'NASDAQ:GOOGL',
-  'NASDAQ:MSFT',
-  'NASDAQ:YHOO',
-  'NYSE:IBM',
-  'NYSE:TWTR'
-];
+// var SYMBOLS = [
+//   'NASDAQ:AAPL',
+//   'NASDAQ:GOOGL',
+//   'NASDAQ:MSFT',
+//   'NASDAQ:YHOO',
+//   'NYSE:IBM',
+//   'NYSE:TWTR'
+// ];
 
-yahooFinance.companyNews({
-  symbols: SYMBOLS
-}, function (err, result) {
-  if (err) { throw err; }
-  _.each(result, function (news, symbol) {
-    console.log(util.format(
-      '=== %s (%d) ===',
-      symbol,
-      news.length
-    ).cyan);
-    if (news[0]) {
-      console.log(
-        '%s\n...\n%s',
-        JSON.stringify(news[0], null, 2),
-        JSON.stringify(news[news.length - 1], null, 2)
-      );
-    } else {
-      console.log('N/A');
-    }
+// yahooFinance.companyNews({
+//   symbols: SYMBOLS
+// }, function (err, result) {
+//   if (err) { throw err; }
+//   _.each(result, function (news, symbol) {
+//     console.log(util.format(
+//       '=== %s (%d) ===',
+//       symbol,
+//       news.length
+//     ).cyan);
+//     if (news[0]) {
+//       console.log(
+//         '%s\n...\n%s',
+//         JSON.stringify(news[0], null, 2),
+//         JSON.stringify(news[news.length - 1], null, 2)
+//       );
+//     } else {
+//       console.log('N/A');
+//     }
   
   
-});
+// });
  
 
 // // Creates the endpoint for our webhook 
